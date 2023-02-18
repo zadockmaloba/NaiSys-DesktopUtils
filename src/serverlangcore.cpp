4abf13d7252eaa413bcdeee816e9a7454932ef3a
Zadock Maloba <malobazadock@gmail.com>
2023-02-18 22:38:23 +0300

    Create serverlangcore class

---
+++ src/serverlangcore.cpp
@@ -0,0 +1,19 @@
+#include "serverlangcore.h"
+#include <QVariant>
+#include <QVariantList>
+
+#include "serverlangfunctions.h"
+
+namespace NaiSys {
+namespace ServerLang {
+
+const QVariant Core::exec(const QString &symbol, const QStringList &args)
+{
+    auto const func = CoreFunctions::functionMap().at(symbol);
+    CoreFunctions::registerParameters(args);
+    func();
+    return 0;
+}
+
+} // namespace ServerLang
+} // namespace NaiSys
---
+++ src/serverlangcore.h
@@ -0,0 +1,51 @@
+#ifndef NAISYS_SERVERLANG_CORE_H
+#define NAISYS_SERVERLANG_CORE_H
+
+#include <QString>
+#include <QMap>
+
+namespace NaiSys {
+namespace ServerLang {
+
+class Core
+{
+    /*enum class CoreFunctions{
+        Println = 1,
+        ReadFile,
+        WriteFile,
+        String_Replace,
+        String_Concat,
+        Struct_Modify,
+        Struct_Insert,
+        Class_Modify,
+        DB_Open,
+        DB_Close,
+        DB_Exec,
+        DB_Write
+    };
+    const QMap<QString, CoreFunctions> m_coreLibMap = {
+        {"Println",       CoreFunctions::Println},
+        {"FileRead",      CoreFunctions::ReadFile},
+        {"FileWrite",     CoreFunctions::WriteFile},
+        {"StringReplace", CoreFunctions::String_Replace},
+        {"StringConcat",  CoreFunctions::String_Concat},
+        {"StructModify",  CoreFunctions::Struct_Modify},
+        {"StructInsert",  CoreFunctions::Struct_Insert},
+        {"ClassModify",   CoreFunctions::Class_Modify},
+        {"DatabaseOpen",  CoreFunctions::DB_Open},
+        {"DatabaseClose", CoreFunctions::DB_Close},
+        {"DatabaseExec",  CoreFunctions::DB_Exec},
+        {"DatabaseWrite", CoreFunctions::DB_Write}
+    };*/
+
+private:
+    Core() = default;
+
+public:
+    static const QVariant exec(const QString &symbol, const QStringList &args = {});
+};
+
+} // namespace ServerLang
+} // namespace NaiSys
+
+#endif // NAISYS_SERVERLANG_CORE_H
