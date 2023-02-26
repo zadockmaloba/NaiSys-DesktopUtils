d5626ae62a9e722307108c450a26a2406c39826d
Zadock Maloba <malobazadock@gmail.com>
2023-02-26 18:04:11 +0300

    return lambda value

--- src/serverlangcore.cpp
+++ src/serverlangcore.cpp
@@ -12,9 +12,9 @@ const QVariant Core::exec(const QString &symbol, const QStringList &args) throw(
     if(auto _mp = CoreFunctions::functionMap(); _mp.find(symbol) != _mp.end()) {
         auto func = _mp[symbol];
         CoreFunctions::registerParameters(args);
-        func();
+        return func();
     }
-    return 0;
+    return {};
 }
 
 const void Core::define(const QString &symbol, const QStringList &params) throw()
