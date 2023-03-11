7cb307f928a4e2cb396c690f700ee5847894551d
Zadock Maloba <malobazadock@gmail.com>
2023-03-11 11:06:30 +0300

    Update python runtime dependancies for MacOS

--- src/libpython.cpp
+++ src/libpython.cpp
@@ -17,7 +17,21 @@ LibPython::~LibPython()
 void LibPython::execute_string(__attribute__((unused)) const char *script)
 {
     try {
-        Py_Initialize();
+        wchar_t *program = Py_DecodeLocale("./python", NULL);
+        wchar_t *pypath =
+                Py_DecodeLocale("./pythonlib", NULL);
+        if (program == NULL) {
+                fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+                exit(1);
+            }
+        Py_SetProgramName(program);
+        Py_SetPythonHome(pypath);
+
+        if(!Py_IsInitialized()) {
+            Py_Initialize();
+        }
+
+        fprintf(stdout, "%s", (const char *)Py_GetPath());
         PyRun_SimpleString(script);
     } catch (...) {
         std::cout << "[PY_SCOPE]: Error running python script";
