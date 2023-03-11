64a63da1b75fb944e921870bba97e56280baf436
Zadock Maloba <malobazadock@gmail.com>
2023-03-11 22:34:44 +0300

    Create LibCore
    Will store custom data structures.

---
+++ src/naisysindexedmap.h
@@ -0,0 +1,21 @@
+#ifndef NAISYS_INDEXED_MAP_H
+#define NAISYS_INDEXED_MAP_H
+
+#include <vector>
+#include <tuple>
+
+namespace NaiSys {
+namespace Core {
+
+template <typename T_ky, typename T_vl>
+class IndexedMap
+{
+public:
+    IndexedMap();
+    ~IndexedMap();
+};
+
+}
+}
+
+#endif //naisysindexedmap.h
