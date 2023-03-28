957e31c89bfa0698098d11fa843b5e27a6ec679e
Zadock Maloba <malobazadock@gmail.com>
2023-03-28 11:35:11 +0300

    Update function call regex.

--- src/serverlanglexer.h
+++ src/serverlanglexer.h
@@ -359,7 +359,7 @@ inline const QRegularExpression Lexer::hook_decl =
 inline const QRegularExpression Lexer::function_decl =
         QRegularExpression{"def\\s+\\w+\\s*\\([\\s\\S]*?\\)*\\s*\\{[^{}]*((\\{[^{}]*\\})|[^{}])*?\\}\\;"};
 inline const QRegularExpression Lexer::function_call =
-        QRegularExpression{"(\\w+\\:\\:)*\\w+\\s*\\([\\s\\S]*?\\)"};
+        QRegularExpression{"(\\w+\\:\\:)*\\w+\\s*\\([^\\(\\)]*((\\([^\\(\\)]*\\))|[^\\(\\)])*?\\)"};
 inline const QRegularExpression Lexer::class_decl =
         QRegularExpression{"class\\s+\\w+[=\\s]*\\{[^{}]*((\\{[^{}]*\\})|[^{}])*?\\}\\;"};
 inline const QRegularExpression Lexer::class_decl_inner =
