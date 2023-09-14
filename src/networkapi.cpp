bff1a648b144172f2bf49e8810b110d55eb17e40
Zadock Maloba <malobazadock@gmail.com>
2023-09-14 10:09:23 +0300

    Implement LibNetwork

---
+++ src/networkapi.cpp
@@ -0,0 +1,62 @@
+#include "networkapi.h"
+#include <QCoreApplication>
+
+NetworkController::NetworkController(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+QNetworkAccessManager* NetworkController::netManager()
+{
+    return m_netManager.get();
+}
+
+const NetworkController::job_map &NetworkController::requestHeap()
+{
+    return m_requestHeap;
+}
+
+void NetworkController::createPushRequest(const QString& path, const QByteArray &data)
+{
+
+}
+
+QByteArray NetworkController::createPushRequestAwait(const QString& path, const QByteArray &data)
+{
+
+}
+
+void NetworkController::createGetRequest(const QString& path, const QByteArray &data)
+{
+    auto reply = netManager()->get(QNetworkRequest("http://"+m_serverHost+":"+QString::number(m_serverPort)+path));
+
+    connect(reply, &QNetworkReply::finished, [reply]() {
+        qDebug() << reply->readAll();
+    });
+}
+
+QByteArray NetworkController::createGetRequestAwait(const QString& path, const QByteArray &data)
+{
+    QByteArray ret;
+    auto reply = netManager()->get(QNetworkRequest("http://"+m_serverHost+":"+QString::number(m_serverPort)+path));
+
+    connect(reply, &QNetworkReply::finished, [reply, &ret]() {
+        ret = reply->readAll();
+    });
+
+    reply->waitForReadyRead(3000);
+
+    while(!reply->isFinished()) {
+        QCoreApplication::processEvents();
+    }
+
+    qDebug() << "Got response";
+
+    return ret;
+}
+
+void NetworkController::onRequestComplete()
+{
+    qDebug() << "Request complete";
+}
---
+++ src/networkapi.h
@@ -0,0 +1,49 @@
+#ifndef NETWORKAPI_H
+#define NETWORKAPI_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+#include <memory>
+#include <map>
+
+class NetworkController : public QObject
+{
+    Q_OBJECT
+public:
+    using netman_ptr = std::unique_ptr<QNetworkAccessManager>;
+    using req_ptr = std::unique_ptr<QNetworkRequest>;
+    using job_map = std::map<int, req_ptr>;
+
+public:
+    explicit NetworkController(QObject *parent = nullptr);
+
+public:
+    static QNetworkAccessManager* netManager();
+    static const job_map &requestHeap();
+    Q_INVOKABLE static void createPushRequest(const QString& path, const QByteArray &data);
+    Q_INVOKABLE static QByteArray createPushRequestAwait(const QString& path, const QByteArray &data);
+    Q_INVOKABLE static void createGetRequest(const QString& path, const QByteArray &data);
+    Q_INVOKABLE static QByteArray createGetRequestAwait(const QString& path, const QByteArray &data);
+
+public slots:
+    void onRequestComplete();
+
+private:
+    static netman_ptr m_netManager;
+    static job_map m_requestHeap;
+    static QString m_serverHost;
+    static int m_serverPort;
+
+signals:
+
+};
+
+inline NetworkController::netman_ptr NetworkController::m_netManager = std::make_unique<QNetworkAccessManager>();
+inline NetworkController::job_map NetworkController::m_requestHeap = {};
+inline QString NetworkController::m_serverHost = "google.com";
+inline int NetworkController::m_serverPort = 80;
+
+#endif // NETWORKAPI_H
