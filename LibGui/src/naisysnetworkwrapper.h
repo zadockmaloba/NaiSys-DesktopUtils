020d8dde40e2d9ca2a646986038e42c5b8a91dbf
Zadock Maloba <malobazadock@gmail.com>
2023-09-25 10:52:17 +0300

    Make wrapper functions in netwrokwrapper invokable.

--- LibGui/src/naisysnetworkwrapper.h
+++ LibGui/src/naisysnetworkwrapper.h
@@ -13,6 +13,65 @@ class NaiSysNetworkWrapper : public NetworkController
     QML_SINGLETON
 public:
     explicit NaiSysNetworkWrapper(QObject *parent = nullptr);
+
+public:
+    Q_INVOKABLE QObject *instance() { return global_net_ctrl; }
+
+    Q_INVOKABLE void createPushRequest(const QString &path, const QByteArray &data)
+    {
+        return NetworkController::createPushRequest(path, data);
+    }
+
+    Q_INVOKABLE QByteArray createPushRequestAwait(const QString &path, const QByteArray &data)
+    {
+        emit requestStarted();
+        auto ret = NetworkController::createPushRequestAwait(path, data);
+        emit requestComplete();
+        return ret;
+    }
+
+    Q_INVOKABLE void createGetRequest(const QString &path, const QByteArray &data = {})
+    {
+        return NetworkController::createGetRequest(path, data);
+    }
+
+    Q_INVOKABLE QByteArray createGetRequestAwait(const QString &path, const QByteArray &data = {})
+    {
+        emit requestStarted();
+        auto ret = NetworkController::createGetRequestAwait(path, data);
+        emit requestComplete();
+
+        return ret;
+    }
+
+    Q_INVOKABLE void createPushRequestSecure(const QString &path, const QByteArray &data)
+    {
+        return NetworkController::createPushRequestSecure(path, data);
+    }
+
+    Q_INVOKABLE QByteArray createPushRequestAwaitSecure(const QString &path, const QByteArray &data)
+    {
+        emit requestStarted();
+        auto ret = NetworkController::createPushRequestAwaitSecure(path, data);
+        emit requestComplete();
+
+        return ret;
+    }
+
+    Q_INVOKABLE void createGetRequestSecure(const QString &path, const QByteArray &data = {})
+    {
+        return NetworkController::createGetRequestSecure(path, data);
+    }
+
+    Q_INVOKABLE QByteArray createGetRequestAwaitSecure(const QString &path,
+                                                       const QByteArray &data = {})
+    {
+        emit requestStarted();
+        auto ret = NetworkController::createGetRequestAwaitSecure(path, data);
+        emit requestComplete();
+
+        return ret;
+    }
 };
 
 #endif // NAISYSNETWORKWRAPPER_H
--- src/networkapi.h
+++ src/networkapi.h
@@ -23,15 +23,15 @@ public:
 public:
     static QNetworkAccessManager* netManager();
     static const job_map &requestHeap();
-    Q_INVOKABLE static void createPushRequest(const QString& path, const QByteArray &data);
-    Q_INVOKABLE static QByteArray createPushRequestAwait(const QString& path, const QByteArray &data);
-    Q_INVOKABLE static void createGetRequest(const QString& path, const QByteArray &data={});
-    Q_INVOKABLE static QByteArray createGetRequestAwait(const QString& path, const QByteArray &data={});
+    static void createPushRequest(const QString &path, const QByteArray &data);
+    static QByteArray createPushRequestAwait(const QString &path, const QByteArray &data);
+    static void createGetRequest(const QString &path, const QByteArray &data = {});
+    static QByteArray createGetRequestAwait(const QString &path, const QByteArray &data = {});
 
-    Q_INVOKABLE static void createPushRequestSecure(const QString& path, const QByteArray &data);
-    Q_INVOKABLE static QByteArray createPushRequestAwaitSecure(const QString& path, const QByteArray &data);
-    Q_INVOKABLE static void createGetRequestSecure(const QString& path, const QByteArray &data={});
-    Q_INVOKABLE static QByteArray createGetRequestAwaitSecure(const QString& path, const QByteArray &data={});
+    static void createPushRequestSecure(const QString &path, const QByteArray &data);
+    static QByteArray createPushRequestAwaitSecure(const QString &path, const QByteArray &data);
+    static void createGetRequestSecure(const QString &path, const QByteArray &data = {});
+    static QByteArray createGetRequestAwaitSecure(const QString &path, const QByteArray &data = {});
 
     static QString serverHost();
     static void setServerHost(const QString &newServerHost);
@@ -41,6 +41,10 @@ public:
 
 public slots:
     void onRequestComplete();
+
+signals:
+    void requestStarted();
+    void requestComplete();
 
 private:
     static netman_ptr m_netManager;
@@ -56,5 +60,7 @@ inline NetworkController::netman_ptr NetworkController::m_netManager = std::make
 inline NetworkController::job_map NetworkController::m_requestHeap = {};
 inline QString NetworkController::m_serverHost = "google.com";
 inline int NetworkController::m_serverPort = 80;
+
+inline static NetworkController *global_net_ctrl = new NetworkController;
 
 #endif // NETWORKAPI_H
